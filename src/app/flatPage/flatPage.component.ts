e2c7cc08d9bc5a618dc3c8d04a004e3bf82250ae
ansokolova <ansokolova@sdvor.com>
2021-06-03 21:23:51 +0500

    переход на страницу квартиры

--- src/app/flatPage/flatPage.component.ts
+++ src/app/flatPage/flatPage.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Flat } from "../flats/flatDataType";
+import { HttpClient } from '@angular/common/http';
+
 
 
 @Component({
-  selector: 'flat-details',
-  templateUrl: './flatPage.component.html',
-  styleUrls: ['./flatPage.component.css'],
+    selector: 'flat-details',
+    templateUrl: './flatPage.component.html',
+    styleUrls: ['./flatPage.component.css'],
 
 })
 
-export class FlatPageComponent  {
+export class FlatPageComponent implements OnInit {
+    flat: Flat | undefined
+
+    constructor(
+        private route: ActivatedRoute,
+        private http: HttpClient
+    ) { }
+
+    ngOnInit() {
+        const routeParams = this.route.snapshot.paramMap;
+        const flatId = Number(routeParams.get('id'));
+        this.http.get(`https://www.sdvor.com/api/common/flats/${flatId}`).subscribe((data: any) => this.flat=data);
+    }
 }
